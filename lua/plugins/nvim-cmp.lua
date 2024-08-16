4cfb409d3a7ff0adb3db8b2cf947cee2c0d1b7e3
Max Abramov <jailthehorny@gmail.com>
2024-08-16 14:03:42 +0300

    Custom sort order for completion items

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -15,6 +15,34 @@ return {
       region_check_events = "InsertEnter"
     })
 
+    local lsp_kind_comparator = function(conf)
+      local lsp_types = require("cmp.types").lsp
+      return function(entry1, entry2)
+        if entry1.source.name ~= "nvim_lsp" then
+          if entry2.source.name == "nvim_lsp" then
+            return false
+          else
+            return nil
+          end
+        end
+
+        local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
+        local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
+
+        local priority1 = conf.kind_priority[kind1] or 0
+        local priority2 = conf.kind_priority[kind2] or 0
+        if priority1 == priority2 then
+          return nil
+        else
+          return priority2 < priority1
+        end
+      end
+    end
+
+    local label_comparator = function(entry1, entry2)
+      return entry1.completion_item.label:len() < entry2.completion_item.label:len()
+    end
+
     cmp.setup({
       snippet = {
         expand = function(args)
@@ -35,9 +63,10 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-d>"] = cmp.mapping.scroll_docs(1),
         ["<C-u>"] = cmp.mapping.scroll_docs(-1),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(1) then
+          if cmp.visible() then
+            cmp.confirm({ select = true })
+          elseif luasnip.jumpable(1) then
             luasnip.jump(1)
           else
             fallback()
@@ -60,7 +89,45 @@ return {
           { name = "buffer", max_item_count = 3 }
         }
       ),
+      sorting = {
+        priority_weight = 1,
+        comparators = {
+          lsp_kind_comparator({
+            kind_priority = {
+              Parameter = 14,
+              Variable = 12,
+              Field = 11,
+              Property = 11,
+              Constant = 10,
+              Enum = 10,
+              EnumMember = 10,
+              Event = 10,
+              Function = 10,
+              Method = 10,
+              Operator = 10,
+              Reference = 10,
+              Struct = 10,
+              File = 8,
+              Folder = 8,
+              Class = 5,
+              Color = 5,
+              Module = 5,
+              Keyword = 2,
+              Constructor = 1,
+              Interface = 1,
+              Snippet = 0,
+              Text = 1,
+              TypeParameter = 1,
+              Unit = 1,
+              Value = 1,
+            },
+          }),
+          label_comparator,
+        }
+      },
       formatting = {
+        expandable_indicator = true,
+        fields = { "abbr", "kind", "menu" },
         format = function(_, item)
           local kind_icons = {
             Text = " ",
