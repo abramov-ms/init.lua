89a2b7911a642a0fd6fe4d2c03856503512b8bae
Max Abramov <jailthehorny@gmail.com>
2024-08-16 14:04:03 +0300

    Show hidden files in oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,13 +1,16 @@
 return {
   'stevearc/oil.nvim',
   dependencies = { "echasnovski/mini.icons" },
-  config = function ()
+  config = function()
     require("oil").setup({
       columns = {
         "permissions",
         "size",
         "mtime",
         "icon",
+      },
+      view_options = {
+        show_hidden = true
       }
     })
   end
