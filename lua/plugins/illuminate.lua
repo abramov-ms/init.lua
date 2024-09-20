b6d6a1c47db853aefd601cc84aafdc5e221a3390
Max Abramov <jailthehorny@gmail.com>
2024-09-20 12:28:38 +0300

    Limit word highliting to LSP

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -2,7 +2,10 @@ return {
   "RRethy/vim-illuminate",
   config = function()
     require("illuminate").configure({
-      delay = 0
+      delay = 0,
+      providers = {
+        "lsp"
+      }
     })
   end
 }
