984b90cc8e8afa3814b5c6d9c59052a9d13c5fb6
Max Abramov <jailthehorny@gmail.com>
2025-01-01 13:43:58 +0300

    Less soy keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,19 +4,6 @@ vim.g.mapleader = " "
 
 vim.keymap.set({ "n", "t" }, "<C-q>", "<Cmd>q!<CR>")
 
--- Moving around windows.
-
-local function focus_window(which)
-  return function()
-    vim.cmd.wincmd(which)
-  end
-end
-
-vim.keymap.set({ "n", "t" }, "<C-j>", focus_window("j"))
-vim.keymap.set({ "n", "t" }, "<C-h>", focus_window("h"))
-vim.keymap.set({ "n", "t" }, "<C-k>", focus_window("k"))
-vim.keymap.set({ "n", "t" }, "<C-l>", focus_window("l"))
-
 -- Resizing windows.
 
 vim.keymap.set("n", "<C-Up>", "<Cmd>resize +1<CR>")
@@ -33,11 +20,6 @@ vim.keymap.set("n", "<Leader>d", [["_d]])
 
 vim.keymap.set("n", "<Leader>x", "<Cmd>silent !chmod +x %<CR>")
 
--- Splits.
-
-vim.keymap.set("n", "<Leader>%", vim.cmd.vs)
-vim.keymap.set("n", [[<Leader>"]], vim.cmd.split)
-
 -- File browser.
 
 vim.keymap.set("n", "-", vim.cmd.Oil)
@@ -45,3 +27,8 @@ vim.keymap.set("n", "-", vim.cmd.Oil)
 -- Lsp.
 
 vim.keymap.set("n", "<M-o>", vim.cmd.ClangdSwitchSourceHeader)
+
+-- Quickfix list.
+
+vim.keymap.set("n", "<C-j>", "<Cmd>cn<CR>zz")
+vim.keymap.set("n", "<C-k>", "<Cmd>cp<CR>zz")
--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -22,3 +22,5 @@ vim.opt.cmdheight = 0
 vim.opt.laststatus = 3
 
 vim.g.nonels_suppress_issue58 = true
+
+vim.opt.colorcolumn = {80}
