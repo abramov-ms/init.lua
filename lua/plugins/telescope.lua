4c5d3e6c4900f8331a1bf2aa357565a3b5a57461
Max Abramov <jailthehorny@gmail.com>
2026-04-22 18:36:16 +0300

    bump telescope version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,8 +6,8 @@ end
 
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = "nvim-lua/plenary.nvim",
+  version = "*",
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = true,
   keys = {
     { "<Leader>ff",  telescope("find_files") },
